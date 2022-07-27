de0cd9b42f9957de22f53c8fcecc33f3fed7a656
Robson Raphael <robsonraphael2001@gmail.com>
2022-07-26 23:32:38 -0300

    Update Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
+import { useState } from "react";
+
+/* Components */
 import { Header } from "./components/header";
 import { Timer } from "./components/timer";
-import { Container } from "./style/global";
+import { Global } from "./style/global";
+import { Tasks } from "./components/tasks";
+
+/* Theme */
 import { ThemeProvider } from "styled-components";
 import { DefaultTheme, ShortTheme, LongTheme } from "./style/theme";
-import { useState } from "react";
 
 function App() {
   const [theme, setTheme] = useState("default");
-
+  const choseTheme = () => {
+    if (theme === "default") return DefaultTheme;
+    if (theme === "short") return ShortTheme;
+    if (theme === "long") return LongTheme;
+  };
   return (
     <>
-      <ThemeProvider
-        theme={
-          theme === "default"
-            ? DefaultTheme
-            : ShortTheme && theme === "short"
-            ? ShortTheme
-            : LongTheme
-        }
-      >
-        <Container>
-          <Header />
-          <Timer
-            isDefault={() => setTheme("default")}
-            isShort={() => setTheme("short")}
-            isLong={() => setTheme("long")}
-          />
-        </Container>
+      <ThemeProvider theme={choseTheme()}>
+        <Header />
+        <Timer
+          isDefault={() => setTheme("default")}
+          isShort={() => setTheme("short")}
+          isLong={() => setTheme("long")}
+        />
+        <Tasks />
+        <Global />
       </ThemeProvider>
     </>
   );
